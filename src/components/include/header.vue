12656ffb69c3658599b09800b17ddc989bf1cdd2
PureMint <30465397+PureMint@users.noreply.github.com>
2017-10-18 09:28:20 +0800

    update

--- src/components/include/header.vue
+++ src/components/include/header.vue
@@ -1,24 +1,4 @@
 <template>
-  <!--<div class="navbar navbar-default navbar-fixed-top">-->
-    <!--<div class="container">-->
-      <!--&lt;!&ndash;<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myCollapse">&ndash;&gt;-->
-      <!--&lt;!&ndash;<span class="icon-bar"></span>&ndash;&gt;-->
-      <!--&lt;!&ndash;<span class="icon-bar"></span>&ndash;&gt;-->
-      <!--&lt;!&ndash;<span class="icon-bar"></span>&ndash;&gt;-->
-      <!--&lt;!&ndash;</button>&ndash;&gt;-->
-      <!--<img src="../../common/image/logo.png" alt="logo"/>-->
-      <!--<div class="collapse navbar-collapse" id="myCollapse">-->
-        <!--<ul class="nav navbar-nav navbar-right">-->
-          <!--<li class="animated zoomIn">首页</li>-->
-          <!--<li class="animated zoomIn">产品</li>-->
-          <!--<li class="animated zoomIn">服务</li>-->
-          <!--<li class="animated zoomIn">关于</li>-->
-          <!--<li class="animated zoomIn">下载</li>-->
-          <!--<li class="animated zoomIn">登录 / 注册</li>-->
-        <!--</ul>-->
-      <!--</div>-->
-    <!--</div>-->
-  <!--</div>-->
   <nav class="navbar navbar-default">
     <div class="container">
       <div class="navbar-header">
